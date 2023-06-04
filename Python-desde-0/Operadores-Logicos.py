c1f3ecb64c742ad76ed49082e65a81ee450f8746
DavidAgudelo09 <davidagudelo0926@gmail.com>
2023-06-04 06:19:15 -0500

    Estudio

---
+++ Python-desde-0/Operadores-Logicos.py
@@ -0,0 +1,15 @@
+# OPERADORES LÓGICOS
+
+# Operador  Descripción
+
+#   and     Se cumple a y b
+#   or      Se cumple a o b
+#   not         No a
+
+#   Ejemplo
+
+a = 30
+b = 40
+c = 50
+r = ((a < b) and (b < c))
+print("Resultado: ", r)
