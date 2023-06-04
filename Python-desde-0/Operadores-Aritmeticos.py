483935d26037537c83115189b57eadd2907b035c
DavidAgudelo09 <davidagudelo0926@gmail.com>
2023-06-04 06:05:59 -0500

    Estudio

--- Python-desde-0/Operadores-Aritmeticos.py
+++ Python-desde-0/Operadores-Aritmeticos.py
@@ -1,14 +1,14 @@
 # OPERADORES ARITMÉTICOS
 
-#Operador   #Descripción    #Ejemplo    #Resultado
-#   +       #    Suma       # C = 3+5     # C = 8
-#   -       #    Resta      # C = 6-4     # C = 2
-#   -       #    Negación   # C = -9      # C = -9
-#   *       #Multiplicación # C = 3*9     # C = 27
-#  **       #   Potencia    # C = 2**3    # C = 8
-#  /        # División      # C = 7/2     # C = 3.5
-# //        #Dvisión entera # C = 7/2     # C = 3.0
-#  %        # Modulo        # C = 9%3     # C = 0
+#Operador   #Descripción      #Ejemplo      #Resultado
+#   +       #    Suma         # C = 3+5     # C = 8
+#   -       #    Resta        # C = 6-4     # C = 2
+#   -       #    Negación     # C = -9      # C = -9
+#   *       # Multiplicación  # C = 3*9     # C = 27
+#  **       #    Potencia     # C = 2**3    # C = 8
+#  /        #    División     # C = 7/2     # C = 3.5
+# //        # Dvisión entera  # C = 7/2     # C = 3.0
+#  %        #    Modulo       # C = 9%3     # C = 0
 
 n1 = 9
 n2 = 3
