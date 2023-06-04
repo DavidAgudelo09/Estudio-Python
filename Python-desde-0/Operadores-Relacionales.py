effdecc40b4bf8357794655bad633e5e02905f53
DavidAgudelo09 <davidagudelo0926@gmail.com>
2023-06-04 06:13:50 -0500

    Estudio

---
+++ Python-desde-0/Operadores-Relacionales.py
@@ -0,0 +1,13 @@
+# OPERADORES RELACIONALES
+
+# Operador   Comparación
+
+#   ==        Igual que
+#   !=        Distinto que
+#   <         Menor que
+#   <=        Menor o igual que
+#   >         Mayor que
+#   >=        Mayor o igual que
+
+r = (120 <= 100)
+print(r)
