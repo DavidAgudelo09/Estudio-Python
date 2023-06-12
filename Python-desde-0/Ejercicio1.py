e07c55829bcf6333bdd5518ae086214011806c76
DavidAgudelo09 <davidagudelo0926@gmail.com>
2023-06-12 17:32:27 -0500

    Estudio

---
+++ Python-desde-0/Ejercicio1.py
@@ -0,0 +1,9 @@
+# Ejercicio 1
+
+a = float(input("Digite el valor de A: "))
+b = float(input("Digite el valor de B: "))
+c = float(input("Digite el valor de C: "))
+
+r = ((c+5)*(b**2-3*a*c)*a**2)/(4*a)
+
+print(f"El resultado es: {r}")
