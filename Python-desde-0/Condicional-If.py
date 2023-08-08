753a80edd336e7d68888d00a20d74cb492d69342
DavidAgudelo09 <davidagudelo0926@gmail.com>
2023-08-08 17:19:55 -0500

    estudio

---
+++ Python-desde-0/Condicional-If.py
@@ -0,0 +1,13 @@
+#Condicional If
+
+dato = int(input("Ingrese un numero: "))
+
+if dato > 0:
+    print("Numero positivo")
+    print("Segundo resultado positivo")
+elif dato == 0:
+    print("Resultado 0")
+else:
+    print("Es negativo")
+
+print("Fin")
